278be05739d099b7696784f8a236b2ab540f3d80
Josh Cavender <jcavende@calpoly.edu>
2024-10-18 20:50:41 -0700

    Final Commit

---
+++ src/main/java/tasktwo/Database.java
@@ -0,0 +1,14 @@
+package tasktwo;
+
+public class Database {
+	
+	public boolean isAvailable() {
+        // TODO implement the access to the database
+        return false;
+    }
+	
+    public int getUniqueId() {
+        return 42;
+    }
+
+}
---
+++ src/test/java/tasktwo/ServiceTest.java
@@ -0,0 +1,19 @@
+package tasktwo;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+class ServiceTest {
+
+  Database databaseMock = Mockito.mock(Database.class);
+  Service service = new Service(databaseMock);
+
+  @Test
+  public void test() {
+    when(databaseMock.getUniqueId()).thenReturn(42);
+    assertEquals("Using database with id: 42", service.getDatabaseID());
+  }
+}
